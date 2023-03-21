19af35e677114befc196232595a98003c473c952
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2023-03-21 02:22:48 +0100

    Started something at least

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import Logo from "../../Assets/IMG/Logo.png";
+
+export const Login = () => {
+  return (
+    <div className="login-container flex-row">
+      <img src={Logo} alt="" className="login-image" />
+    </div>
+  );
+};
